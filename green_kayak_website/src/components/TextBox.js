da5d2cbc345ff3202f04683ce5e51ece88feed06
Mikkel Andersen <mikkhel@gmail.com>
2021-11-16 11:14:59 +0100

    Changed the textbox component

--- green_kayak_website/src/components/TextBox.js
+++ green_kayak_website/src/components/TextBox.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class TextBox extends Component {
-
+function TextBox(props) {
+    return(
+        <div className="text-box">
+            <p>{props.text}</p>
+        </div>
+    )
 }
 
 export default TextBox
