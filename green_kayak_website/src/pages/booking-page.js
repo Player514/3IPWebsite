af00df218e11132c00893cdafe300a95ae9eaba5
Mikkel Andersen <mikkhel@gmail.com>
2021-11-24 09:45:44 +0100

    Finishing up the booking page

--- green_kayak_website/src/pages/booking-page.js
+++ green_kayak_website/src/pages/booking-page.js
@@ -9,17 +9,21 @@ function BookingPage() {
 
     return (
       <div className="booking-page">
-        <div className="booking-page-content">
-        <h1>Booking</h1>
+        <div className="booking-page-text">
+        <h1>BOOKING</h1>
         <h3>Location: LETZ SUSHI Christianshavn, Overgarden Neden Vandet 41, 1414 København K </h3>
-        <p>Name</p>
-        <TextField type={"text"} placeholder={"Enter your name"}/>
-        <p>E-mail</p>
-        <TextField type={"email"} placeholder={"Enter your e-mail"}/>
-        <p>Phonenumber</p>
-        <TextField type={"number"} placeholder={"Enter your phonenumber"}/> 
-        <button onClick={() => navigate('/before-arrival')}>Next</button>
         </div>
+          <div className="text-field-area">
+            <p>Name</p>
+            <TextField type={"text"} placeholder={"Enter your name"}/>
+            <p>E-mail</p>
+            <TextField type={"email"} placeholder={"Enter your e-mail"}/>
+            <p>Phonenumber</p>
+            <TextField type={"number"} placeholder={"Enter your phonenumber"}/> 
+          </div>
+          <div className="button-area">
+          <button onClick={() => navigate('/before-arrival')}>Next</button>
+          </div>
       </div>
     );
   }
